72367fe510725c63343ff4befd0e12a56b67e565
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-03-28 15:06:39 -0400

    Created the Like and Dislike button logic to toggle the like and dislike statuses

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
+import './Post.css';
 
 const Post = props => {
+    const [likedStatus, setLikedStatus] = useState('inactive');
+    const [dislikedStatus, setDislikedStatus] = useState('inactive');
+
+    const handleLike = () => {
+        if (likedStatus === 'inactive') setLikedStatus('active');
+        else setLikedStatus('inactive');
+        setDislikedStatus('inactive');
+    };
+
+    const handleDislike = () => {
+        if (dislikedStatus === 'inactive') setDislikedStatus('active');
+        else setDislikedStatus('inactive');
+        setLikedStatus('inactive');
+    };
+
+
+
+    return(
     <div>
         <h3>{props.post.name}</h3>
         <p>{props.post.post}</p>
-        <button>Like</button>
-        <button>Dislike</button>
+        <button className={likedStatus} onClick={handleLike}>Like</button>
+        <button className={dislikedStatus} onClick={handleDislike}>Dislike</button>
     </div>
-}
+    );
+};
  
 export default Post;
